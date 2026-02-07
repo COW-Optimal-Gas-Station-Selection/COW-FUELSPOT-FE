e4da7b92fcd1820d40cdbaf2bb10f23d8e827e17
leesj0188 <lsj01881@gmail.com>
2026-02-08 00:41:52 +0900

    <Fix>: 모바일 버전 헤더 수정

--- FUELSPOT/src/pages/mypage/molecules/SecurityActions.jsx
+++ FUELSPOT/src/pages/mypage/molecules/SecurityActions.jsx
@@ -2,11 +2,11 @@ import Button from '../../../components/Button';
 
 export default function SecurityActions({ onPasswordChange, onDeleteAccount }) {
   return (
-    <div className="pt-6 flex flex-col gap-6">
+    <div className="pt-6 flex flex-col gap-3">
       <Button 
         onClick={onPasswordChange} 
         variant="outline" 
-        className="h-12 text-blue-600 border-blue-100 hover:bg-blue-50 font-bold"
+        className="w-full h-12 text-base font-bold text-blue-600 border-blue-100 hover:bg-blue-50"
       >
         비밀번호 변경하기
       </Button>
